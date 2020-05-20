9f588003c37b856496792063756806eae41d2e7d
jasanishubh123 <jasanishubham@gmail.com>
2020-05-21 04:05:12 +0530

    Add Componenet and css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,7 +9,8 @@ import './App.css';
     persons:[
       {name:'shubham',age:22},
       {name:'Nency',age:15}
-    ]
+    ],
+    showPerson:false
   });
 
  const checkbtn=()=>{
@@ -18,17 +19,61 @@ import './App.css';
       persons:[
         {name:'shubh',age:22},
         {name:'Nena',age:14}
-      ]
+      ],
+      showPerson:false
     });
   };
 
+  const checkchangebtn= (event) =>{
+    setPersonState({
+      persons:[
+        {name:event.target.value,age:22},
+        {name:"Nena",age:14}
+      ],
+      showPerson:true
+    })
+  }
+  const togglepara=()=>{
+      const mystate=personState.showPerson;
+      setPersonState({
+        persons:[
+          {name:"Shubham",age:22},
+          {name:"Nena",age:14}
+        ],
+        showPerson:!mystate 
+      })
+
+  }
+  
+
+  const getper = ()=>{
+    
+    if(personState.showPerson){
+     return(
+        <div>
+        {/* <Person change={checkchangebtn} name={personState.persons[0].name} age={personState.persons[0].age}>My Hobbies is - Coding </Person>
+        <Person name={personState.persons[1].name} age={personState.persons[1].age}/> */}
+          {
+            personState.persons.map(p=>{
+              return (
+                <Person name={p.name} age={p.age} />
+              );
+            })
+          }
+      </div>
+     ) 
+    }
+    
+
+  }
  
     return (
+
       <div className="App">
         <h1>Hello This is my first React App Yeyyyy.</h1>
-        <button onClick={checkbtn} >Switch names</button>
-        <Person name={personState.persons[0].name} age={personState.persons[0].age}>My Hobbies is - Coding </Person>
-        <Person name={personState.persons[1].name} age={personState.persons[1].age}/>
+        <button onClick={togglepara} >Toggle Persons</button>
+        {getper()}
+        
       </div>
     );
     
--- react-app/src/Public/Person.js
+++ react-app/src/Public/Person.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import '../Public/Person.css'
 
 
 const person =(attr)=>{
 
-return (
-    <div>
-        <p>Hey, {attr.name} { attr.age } How Are You??? and {attr.children}</p>
-
-    </div>
-    
-)
+    return (
+        <div >
+            <p className="Person">Hey, {attr.name} { attr.age } How Are You??? and {attr.children}</p>
+            <input type="text" value={attr.name} onChange={attr.change} />
+        </div>
+        
+    )
 
 }
 export default person;
